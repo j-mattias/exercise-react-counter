1317d29c8a42a8c6f0ad47174900fee342aafc35
Mattias <mattiasjohansson.vfx@gmail.com>
2024-08-21 10:05:05 +0200

    React counter code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <section className="counter-container">
+        <h1>Counter</h1>
+        <p className="counter">{count}</p>
+
+        <div className="buttons-wrapper">
+          <button className="counter__increase" onClick={() => setCount((count) => count + 1)}>
+            +
+          </button>
+          <button className="counter__decrease" onClick={() => setCount((count) => count - 1)}>
+            -
+          </button>
+        </div>
+        <button className="counter__reset" onClick={() => setCount((count) => count = 0)}>
+          Reset
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
